6a128c9e5bf7ac573bbce45765b6ee2018cf4508
James <42086157+puddinator@users.noreply.github.com>
2023-05-22 17:39:59 +0800

    add content of new style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,27 +6,21 @@ const Home: React.FC = () => {
     <IonPage style={{ background: "white", display: "flex" }}>
       <div className="header">
         <h3 className="header-title ion-margin-start">
-          Welcome <br />
-          Back!
+          Sign In <br />
+          to Athena
         </h3>
       </div>
       <div className="body">
         <form className="ion-padding">
-          <h4 className="form-title">Sign In To Athena</h4>
-
           <div className="form-content">
             <IonItem color="#f8f8f8">
-              <IonLabel position="floating">Username</IonLabel>
+              <IonLabel>Username</IonLabel>
               <IonInput />
             </IonItem>
             <IonItem color="#f8f8f8">
-              <IonLabel position="floating">Password</IonLabel>
+              <IonLabel>Password</IonLabel>
               <IonInput type="password" />
             </IonItem>
-
-            <p>
-              <a href="#">Forgot Password?</a>
-            </p>
           </div>
 
           <IonButton
@@ -37,11 +31,14 @@ const Home: React.FC = () => {
           >
             Sign In
           </IonButton>
-        </form>
 
-        <p className="footer">
-          New User? <a href="#">Contact Us</a>
-        </p>
+          <p>
+            <a href="#">Forgot Password?</a>
+          </p>
+        </form>
+      </div>
+      <div className="footer">
+        New User? <a href="#">Contact Us</a>
       </div>
     </IonPage>
   );
