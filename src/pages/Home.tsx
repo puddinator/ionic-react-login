e810d6f09051a58401ddca65be8c7b4cc25ebd0b
James <42086157+puddinator@users.noreply.github.com>
2023-05-22 13:51:28 +0800

    version 1 styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonButton, IonInput, IonItem, IonLabel, IonPage } from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+    <IonPage style={{ background: "white", display: "flex" }}>
+      <div className="header">
+        <h3 className="header-title ion-margin-start">
+          Welcome <br />
+          Back!
+        </h3>
+      </div>
+      <div className="body">
+        <form className="ion-padding">
+          <h4 className="form-title">Sign In To Athena</h4>
+
+          <div className="form-content">
+            <IonItem color="#f8f8f8">
+              <IonLabel position="floating">Username</IonLabel>
+              <IonInput />
+            </IonItem>
+            <IonItem color="#f8f8f8">
+              <IonLabel position="floating">Password</IonLabel>
+              <IonInput type="password" />
+            </IonItem>
+
+            <p>
+              <a href="#">Forgot Password?</a>
+            </p>
+          </div>
+
+          <IonButton
+            className="ion-margin-top form-submit"
+            size="large"
+            type="submit"
+            expand="block"
+          >
+            Sign In
+          </IonButton>
+        </form>
+
+        <p className="footer">
+          New User? <a href="#">Contact Us</a>
+        </p>
+      </div>
     </IonPage>
   );
 };
