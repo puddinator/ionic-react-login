5c5297de99940b3c4c85876c7c0f8d671edb2b51
James <42086157+puddinator@users.noreply.github.com>
2023-05-23 17:01:14 +0800

    add splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,27 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'login-app',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "login-app",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "large",
+      spinnerColor: "#999999",
+      showSpinner: true,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
