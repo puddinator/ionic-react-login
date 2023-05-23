72633c3dd2f951165768e3e5f6bc2902093be707
James <42086157+puddinator@users.noreply.github.com>
2023-05-23 16:28:13 +0800

    update to new style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,62 @@
-import { IonButton, IonInput, IonItem, IonLabel, IonPage } from "@ionic/react";
+import {
+  IonButton,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+} from "@ionic/react";
 import "./Home.css";
 
 const Home: React.FC = () => {
   return (
-    <IonPage style={{ background: "white", display: "flex" }}>
+    <IonPage className="page-container">
+      <IonImg className="leaf2" src="assets/images/leaf-2.png" alt="Leaf" />
+      <IonImg className="leaf6" src="assets/images/leaf-6.png" alt="Leaf" />
+
       <div className="header">
-        <h3 className="header-title ion-margin-start">
-          Sign In <br />
-          to Athena
-        </h3>
+        <h3 className="header-title">Sign In</h3>
+        <h4 className="header-subtitle">to Athena</h4>
       </div>
+
       <div className="body">
-        <form className="ion-padding">
+        <form>
           <div className="form-content">
             <IonItem color="#f8f8f8">
-              <IonLabel>Username</IonLabel>
+              <IonLabel color="#f8f8f8" position="stacked">
+                Username
+              </IonLabel>
               <IonInput />
             </IonItem>
             <IonItem color="#f8f8f8">
-              <IonLabel>Password</IonLabel>
+              <IonLabel color="#f8f8f8" position="stacked">
+                Password
+              </IonLabel>
               <IonInput type="password" />
             </IonItem>
           </div>
 
           <IonButton
-            className="ion-margin-top form-submit"
-            size="large"
             type="submit"
-            expand="block"
+            color="secondary"
+            shape="round"
+            className="form-submit"
           >
             Sign In
           </IonButton>
-
-          <p>
-            <a href="#">Forgot Password?</a>
-          </p>
+          <a href="#">Forgot Password?</a>
         </form>
       </div>
+
       <div className="footer">
-        New User? <a href="#">Contact Us</a>
+        <IonImg
+          className="logo"
+          src="assets/images/logo.png"
+          alt="Beco Ventures"
+        />
+        <p>
+          New User? <a href="#">Contact Us</a>
+        </p>
       </div>
     </IonPage>
   );
